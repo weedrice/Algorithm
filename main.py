52d719c81f726b6c891f47b4c78b5b5076dfed97
weedrice <supp0rtyoo>
2020-05-12 14:52:57 +0900

    #200512 Account Test

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-def solution(inputString):
-    stack = []
-    count = 0
-    for item in inputString:
-        if item == "(" or "[" or "<" or "{":
-            stack.append(item)
-        elif item == ")" or "]" or ">" or "}":
-            try:
-                stack.pop()
-                count = count + 1
-            except IndexError:
-                return -1
-
-    if len(stack) == 0:
-        return count
+def str_len():
+    a = "Life is too short"
+    len(a)
